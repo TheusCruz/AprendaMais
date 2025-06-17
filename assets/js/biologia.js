7317574f2e442debaeddad18a840be45cd808583
TheusCruz <matheuscruz2105@gmail.com>
2025-06-16 21:53:13 -0400

    Add files via upload

---
+++ assets/js/biologia.js
@@ -0,0 +1,48 @@
+const openModal = (valor) => {
+    const modal = document.querySelector('.modal-container')
+    const h1 = document.querySelector('h1')
+    const h6 = document.querySelector('h6')
+    const opicao1 = document.querySelector('.opicao1')
+    const opicao2 = document.querySelector('.opicao2')
+    const texto1 = document.querySelector('.texto1')
+    const texro2 = document.querySelector('.texto2')
+    modal.classList.add('mostrar')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'modal-container' || e.target.id == "fechar") {
+            modal.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container'
+        }
+    })
+
+    if (valor == 1) {
+
+        h1.innerText = 'Resultado da conta:'
+        h6.innerText = 'Juninho foi da festa junina com a Maria, ele tinha 100 reais para gastar, ele acabou comprando 60 reais em comidas, mais depois disso ele ganhou um terço desse valor, logo depois ele compra um presente que custa metade do valor inicial. Quantos reais ira sobrar para Juninho'
+        texto1.innerText = '10 reais'
+        texro2.innerText = '30 reais'
+
+        opicao1.addEventListener('change', (event) => {
+            modal.style.backgroundColor = red;
+        });
+
+    } else if (valor === '2') {
+
+    } else if (valor === '3') {
+
+    } else if (valor === '4') {
+
+    } else if (valor === '5') {
+
+    } else if (valor === '6') {
+
+    } else if (valor === '7') {
+
+    } else if (valor === '8') {
+
+    } else if (valor === '9') {
+
+    } else if (valor === '10') {
+
+    }
+}
